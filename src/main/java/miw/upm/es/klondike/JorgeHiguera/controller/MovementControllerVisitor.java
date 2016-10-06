8e9f44427f2689dede716e9c0009d5c69580c6ec
Jorge Higuera <jorge.higuera.fombuena@gmail.com>
2016-10-06 23:57:29 +0200

    Borrados y renombrados ficheros que no tenian sentido

--- src/main/java/miw/upm/es/klondike/JorgeHiguera/controller/MovementControllerVisitor.java
+++
@@ -1,9 +0,0 @@
-package miw.upm.es.klondike.JorgeHiguera.controller;
-
-public interface MovementControllerVisitor {
-
-	void visit(MovementStrightController movementStrightController);
-	
-	void visit(MovementSuitController movementSuitController);
-	
-}
